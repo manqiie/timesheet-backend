4b52f473a98f65c384c45f0f1b3c652d4bdb9a32
manqiie <qieteo111@gmail.com>
2025-09-15 16:37:29 +0800

    Initial commit

---
+++ timesheet-backend/src/test/java/com/goldtech/timesheet_backend/TimesheetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldtech.timesheet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
